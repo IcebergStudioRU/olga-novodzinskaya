b9a7028cfd0f295899f4e10e79674e4128fe1607
Vitaliy Malyshev <zich.contemporary@mail.ru>
2022-10-19 12:45:44 +0500

    Create components of stylized tags h1, h2, h3

---
+++ src/components/Header1.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Header1 = ({ color, className, children }) => {
+  return (
+    <h1 className={`font-Lobster text-3xl ${color} ${className}`}>
+      {children}
+    </h1>
+  );
+};
+
+export default Header1;
---
+++ src/components/Header2.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Header2 = ({ color, className, children }) => {
+  return (
+    <h2 className={`font-Lobster text-3xl ${color} ${className}`}>
+      {children}
+    </h2>
+  );
+};
+
+export default Header2;
